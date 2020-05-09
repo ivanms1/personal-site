fb57de323f988a0a1c4bee67065c013fbbbbf7a3
Ivan Saldano <ivanms1@gmail.com>
2020-05-09 00:36:15 -0400

    portfolio

--- components/SecondaySection/Portfolio.tsx
+++ components/SecondaySection/Portfolio.tsx
@@ -1,9 +1,47 @@
 import React from 'react';
 
+import styles from './Portfolio.module.css';
+
+const projectsLeft = [
+  'https://source.unsplash.com/random/400x650',
+  'https://source.unsplash.com/random/500x500',
+  'https://source.unsplash.com/random/600x400',
+  'https://source.unsplash.com/random/450x600',
+];
+
+const projectsRight = [
+  'https://source.unsplash.com/random/400x420',
+  'https://source.unsplash.com/random/400x450',
+  'https://source.unsplash.com/random/400x550',
+  'https://source.unsplash.com/random/400x500',
+];
+
 const Portfolio = () => {
   return (
     <div>
-      <h1>Portfolio</h1>
+      <h1 className={styles.Title}>Portfolio</h1>
+      <div className={styles.MainContainer}>
+        <div className={styles.Left}>
+          {projectsLeft.map((project) => (
+            <div key={project} className={styles.Project}>
+              <div className={styles.ImageContainer}>
+                <img src={project} alt='random' />
+              </div>
+              <p>Some Name</p>
+            </div>
+          ))}
+        </div>
+        <div className={styles.Right}>
+          {projectsRight.map((project) => (
+            <div key={project} className={styles.Project}>
+              <div className={styles.ImageContainer}>
+                <img src={project} alt='random' />
+              </div>
+              <p>Some Name</p>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -14,7 +14,7 @@ export interface SectionProps {
 const Home = () => {
   const [currentSection, setCurrentSection] = useState<
     SectionProps['currentSection']
-  >('resume');
+  >('portfolio');
 
   const handleSideBarClick = (section: SectionProps['currentSection']) => {
     setCurrentSection(section);
